e65d62cb9bbf960d49b1ca1dcb4a840952b7894c
Ruben David <rubendavid.rd21@gmail.com>
2026-07-07 22:24:48 -0400

    feat(layout): clean vite boilerplate and set simple centered app layout

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-[#313338] text-white font-sans antialiased">
+      <h1 className="text-4xl font-bold tracking-wide">Clon Discord</h1>
+    </div>
+  );
+}
+
+export default App;
