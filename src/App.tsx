5f6896570cd4593337fb729a7599bd23a4e8e944
ignaccio7 <nacho260298@gmail.com>
2026-07-10 11:37:32 -0400

    feat: add new content for description

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-[#313338] text-white font-sans antialiased">
-      <h1 className="text-4xl font-bold tracking-wide">Clon Discord V44123456</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi labore accusantium numquam voluptatum temporibus blanditiis incidunt eum mollitia voluptatibus libero harum non ut suscipit corporis, dolore molestias optio dicta quaerat!</p>
+      <h1 className="text-4xl font-bold tracking-wide">
+        Clon Discord V44123456
+      </h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi labore
+        accusantium numquam voluptatum temporibus blanditiis incidunt eum
+        mollitia voluptatibus libero harum non ut suscipit corporis, dolore
+        molestias optio dicta quaerat!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi labore
+        accusantium numquam voluptatum temporibus blanditiis incidunt eum
+        mollitia voluptatibus libero harum non ut suscipit corporis, dolore
+        molestias optio dicta quaerat!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi labore
+        accusantium numquam voluptatum temporibus blanditiis incidunt eum
+        mollitia voluptatibus libero harum non ut suscipit corporis, dolore
+        molestias optio dicta quaerat!
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi labore
+        accusantium numquam voluptatum temporibus blanditiis incidunt eum
+        mollitia voluptatibus libero harum non ut suscipit corporis, dolore
+        molestias optio dicta quaerat!
+      </p>
     </div>
   );
 }
