ccacc89cab6bb5b80eb82354c72a8c5892bb3f7b
ignaccio7 <nacho260298@gmail.com>
2026-07-09 23:56:02 -0400

    feat: change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-[#313338] text-white font-sans antialiased">
-      <h1 className="text-4xl font-bold tracking-wide">Clon Discord V4</h1>
+      <h1 className="text-4xl font-bold tracking-wide">Clon Discord V44</h1>
     </div>
   );
 }
